51fffc34ffb78e19353ac99740aabf2764ce9524
Cleiyton Oliveira <cleiton.oliveira@polisys.com.br>
2020-09-13 00:43:36 -0300

    Criação do MidiasRepository, e Criação do Response

---
+++ src/main/java/projeto/SambaTech/repositories/MidiasRepository.java
@@ -0,0 +1,8 @@
+package projeto.SambaTech.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projeto.SambaTech.entities.Midias;
+
+public interface MidiasRepository extends JpaRepository<Midias, Long> {
+
+}
