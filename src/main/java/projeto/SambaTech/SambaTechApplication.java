d00d3cdd41cd2156aa04dff01ad273a84f6cb75a
Cleiyton Oliveira <cleiton.oliveira@polisys.com.br>
2020-09-13 00:12:25 -0300

    Projeto inicial rodando

---
+++ src/main/java/projeto/SambaTech/SambaTechApplication.java
@@ -0,0 +1,13 @@
+package projeto.SambaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/SambaTech/SambaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.SambaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SambaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
