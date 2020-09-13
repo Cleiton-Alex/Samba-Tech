85f50f9b474379908a1a08507c712f17cc1d9164
Cleiyton Oliveira <cleiton.oliveira@polisys.com.br>
2020-09-13 02:12:52 -0300

    Criando a assinaturas dos MidiasService que foram implemetados na MidiasServiceImpl

--- src/main/java/projeto/SambaTech/services/MidiasService.java
+++ src/main/java/projeto/SambaTech/services/MidiasService.java
@@ -1,4 +1,37 @@
 package projeto.SambaTech.services;
 
+import projeto.SambaTech.entities.Midias;
+
+import java.util.Optional;
+
 public interface MidiasService {
+
+    Midias persiste(Midias midias);
+
+    /**
+     * Buscar e retorna todas as Midias ou midias que nao foram Deletadas
+     *
+     * @param deleted
+     * @return Optional<Midias>
+     */
+    Optional<Midias> buscarPorMidiasNaoDeletadas(Boolean deleted);
+
+
+    /**
+     * Buscar e retorna uma Midia dado o id
+     *
+     * @param id
+     * @return Optional<Midias>
+     */
+    Optional<Midias> buscarPorMidiasID(Long id);
+
+
+    /**
+     * Removo uma midia dado o id
+     *
+     * @param id
+     * */
+    void remover(Long id);
 }
+
+
--- src/main/java/projeto/SambaTech/services/impl/MidiasServiceImpl.java
+++ src/main/java/projeto/SambaTech/services/impl/MidiasServiceImpl.java
@@ -1,8 +1,45 @@
 package projeto.SambaTech.services.impl;
 
+import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import projeto.SambaTech.entities.Midias;
+import projeto.SambaTech.repositories.MidiasRepository;
 import projeto.SambaTech.services.MidiasService;
+import sun.rmi.runtime.Log;
+
+import java.util.Optional;
 
 public class MidiasServiceImpl implements MidiasService {
+    private  static final Logger log = LoggerFactory.getLogger(MidiasServiceImpl.class);
+
+    @Autowired
+    private MidiasRepository midiasRepository;
+
+    @Override
+    public Midias persiste(Midias midias) {
+        log.info("Persistindo Lancamento por ID {}", midias);
+        return this.midiasRepository.save(midias);
+    }
+
+    @Override
+    public Optional<Midias> buscarPorMidiasNaoDeletadas( Boolean deleted) {
+        log.info("Buscar Todas as midias ou midias que nao foram deletadas", deleted);
+        return Optional.ofNullable(this.midiasRepository.findByDeleted(deleted));
+    }
+
+    @Override
+    public Optional<Midias> buscarPorMidiasID(Long id) {
+        log.info("Buscar midia por ID {}", id);
+        Optional byId = Optional.ofNullable(this.midiasRepository.findById(id));
+        return byId;
+    }
+
+    @Override
+    public void remover(Long id) {
+
+    }
 
 
 }
