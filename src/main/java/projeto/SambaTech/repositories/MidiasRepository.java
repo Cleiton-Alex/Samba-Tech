4287840708e93a3dd100d0b24d7ba73cb4040b5b
Cleiyton Oliveira <cleiton.oliveira@polisys.com.br>
2020-09-13 02:10:43 -0300

    Criando assinatura do metodo de busca por midias deleted no repositorio

--- src/main/java/projeto/SambaTech/repositories/MidiasRepository.java
+++ src/main/java/projeto/SambaTech/repositories/MidiasRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import projeto.SambaTech.entities.Midias;
 
 public interface MidiasRepository extends JpaRepository<Midias, Long> {
-
+    Midias findByDeleted(Boolean deleted);
 }
